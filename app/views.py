e5ae1a697550ab7cbf224e57f4acfa13515ccfe7
anthonysong <anthonysong666@gmail.com>
2018-06-20 01:23:01 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -4,14 +4,18 @@ from flask import send_file,Flask,request,make_response,render_template,redirect
 from app import  app
 from .forms import rateForm
 
-
+#路由装饰器
 @app.route('/rate',methods=['GET','POST'])
 def rate():
+    #实例化一个表单
     form=rateForm()
+    #处理POST
     if form.submit.data:
         zjh=form.zjh.data
         mm=form.mm.data
         options=webdriver.FirefoxOptions()
+        
+        #linux服务器无显示，设置headless
         options.set_headless()
         options.add_argument('--disable-gpu')
         broswer=webdriver.Firefox(firefox_options=options)
@@ -24,6 +28,7 @@ def rate():
         for i in range(1,100):
             try:
                 broswer.find_element_by_xpath('/html/body/form/table[4]/tbody/tr/td/table/tbody/tr['+str(i)+']/td[5]/img').click()
+                #已经评教则返回前一页
                 if '提交' not in broswer.page_source:
                     broswer.get('http://zhjw.scu.edu.cn/jxpgXsAction.do?oper=listWj')
                     broswer.find_element_by_css_selector(".pageAlign > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > select:nth-child(5) > option:nth-child(8)").click()
@@ -32,6 +37,7 @@ def rate():
                 continue
             ckb=".fieldsettop > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child({}) > td:nth-child(1) > input:nth-child(1)"
             for i in range(2,23,3):
+                #助教和教师评分项不一样的，老师为7，助教为6
                 try:
                     broswer.find_element_by_css_selector(ckb.format(i)).click()
                 except Exception:
@@ -39,13 +45,15 @@ def rate():
             broswer.find_element_by_name('zgpj').send_keys('很好')
             broswer.find_element_by_css_selector('.fieldsettop > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(4) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > img:nth-child(1)').submit()
             try:
-                broswer.find_element_by_xpath("//*[@id='alert']/input").click()
+                broswer.find_element_by_xpath("//*[@id='alert']/input").click()#处理alert
             except Exception:
                 pass
             broswer.get('http://zhjw.scu.edu.cn/jxpgXsAction.do?oper=listWj')
             broswer.find_element_by_css_selector(".pageAlign > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > select:nth-child(5) > option:nth-child(8)").click()
         flash('评教完成')
+        #关闭浏览器进程
         broswer.close()
         return render_template('rate.html',form=form)
+    #处理GET，返回渲染页面
     return render_template('rate.html',form=form)
 
